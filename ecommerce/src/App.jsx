2156a5133985ad582ce8d00530786b1450ec6e3b
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-27 22:39:15 +0530

    Feat: NavBar added

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,12 @@
+import {NavBar} from './components/navigationBar/index';
+import {Hero} from './components/heroImage/index'
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
