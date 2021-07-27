42300fe7b56196d3b1e7a5434bc5bfcc23b6d9ef
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-27 22:41:21 +0530

    style: added hero

---
+++ ecommerce/src/components/heroImage/index.js
@@ -0,0 +1,11 @@
+import './hero.css';
+import main from './Images/main.jpg'
+export function Hero(){
+    return (
+        <div style={{
+            background: `url(${main})`,
+        }}>
+            asdds
+        </div>
+    )
+}
