39f95cc384d3b7a8915cf6c03dfd316a62b87380
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-28 23:32:37 +0530

    fix: added index.js for exporting the components

---
+++ ecommerce/src/components/heroImage/heroImage.js
@@ -0,0 +1,13 @@
+import './hero.css';
+export function Hero(){
+    return (
+        <div className='heroimg'>
+           <h1>JungleClap</h1>
+           <small>A One Stop Destination For Electronic Gadgets & fashion Products </small>
+           <h1>Big Billon Day's are back</h1>
+           <small>(14-16 of August)</small>
+           <p>Offers On All Selected Items*</p>
+           <button className='btn-shp'>Shop Now</button>
+        </div>
+    )
+}
--- ecommerce/src/components/heroImage/index.js
+++ ecommerce/src/components/heroImage/index.js
@@ -1,13 +1,1 @@
-import './hero.css';
-export function Hero(){
-    return (
-        <div className='heroimg'>
-           <h1>JungleClap</h1>
-           <small>A One Stop Destination For Electronic Gadgets </small>
-           <h1>Big Billon Day's are back</h1>
-           <small>(14-16 of August)</small>
-           <p>Offers On All Selected Items*</p>
-           <button className='btn-shp'>Shop Now</button>
-        </div>
-    )
-}
+export {Hero} from './heroImage'
