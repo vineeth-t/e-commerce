9457f4d97ffd3ea0f7cce453c3a14ab17006d382
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-28 14:57:05 +0530

    feat: Added hero for the site

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,10 +1,12 @@
 import {NavBar} from './components/navigationBar/index';
-import {Hero} from './components/heroImage/index'
+import {Hero} from './components/heroImage/index';
+import {ShoppingCatergory} from './components/productCatergories/index'
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero/>
+      <ShoppingCatergory/>
     </div>
   );
 }
--- ecommerce/src/components/heroImage/index.js
+++ ecommerce/src/components/heroImage/index.js
@@ -1,11 +1,13 @@
 import './hero.css';
-import main from './Images/main.jpg'
 export function Hero(){
     return (
-        <div style={{
-            background: `url(${main})`,
-        }}>
-            asdds
+        <div className='heroimg'>
+           <h1>JungleClap</h1>
+           <small>A One Stop Destination For Electronic Gadgets </small>
+           <h1>Big Billon Day's are back</h1>
+           <small>(14-16 of August)</small>
+           <p>Offers On All Selected Items*</p>
+           <button className='btn-shp'>Shop Now</button>
         </div>
     )
 }
