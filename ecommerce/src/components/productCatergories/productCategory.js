035452c01a56504f4875a68953bc7390ee8d6c51
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-29 19:52:18 +0530

    Style: added transition effect on nav

--- ecommerce/src/components/productCatergories/productCategory.js
+++ ecommerce/src/components/productCatergories/productCategory.js
@@ -9,34 +9,34 @@ import sony from './catergoryImages/sony.jpg'
 import './productCatergory.css'
 export function ShoppingCatergory(){
     return(
-        <>
+        <div>
             <h2 className='categories-heading'>Featured Categories</h2>
             <div class='item-catergories'>
-            <a className='catergory-img' style={{background:`url(${camera})no-repeat center/100% 100%`}}>
-                <span>Cameras</span>
-                <span>Flat 30%</span>
-            </a>
-            <a className='catergory-img' style={{background:`url(${fashion})no-repeat center/100% 100%`}}>
-              <span>Tshirts</span>
-                <span>Buy 3 @ Rs/-999</span>
-            </a>
-            <a className='catergory-img' style={{background:`url(${music})no-repeat center/100% 100%`}}>
-            <span>Earpods</span>
-                <span>1+1 Offer(Limted Offer)</span>
-            </a>
+                    <a className='catergory-img' style={{background:`url(${camera})no-repeat center/100% 100%`}}>
+                        <span>Cameras</span>
+                        <span>Flat 30%</span>
+                    </a>
+                    <a className='catergory-img' style={{background:`url(${fashion})no-repeat center/100% 100%`}}>
+                    <span>Tshirts</span>
+                        <span>Buy 3 @ Rs/-999</span>
+                    </a>
+                    <a className='catergory-img' style={{background:`url(${music})no-repeat center/100% 100%`}}>
+                    <span>Earpods</span>
+                        <span>1+1 Offer(Limted Offer)</span>
+                    </a>
             </div>
             <div>
                 <h2 className='categories-heading'>Featured Brands</h2>
                 <div class='brand-types'>
-                <a className='catergory-img brand' style={{background:`url(${apple})no-repeat center/100% 100%`}}/>
-                <a className='catergory-img brand' style={{background:`url(${canon})no-repeat center/100% 100%`}}/>
-                <a className='catergory-img brand' style={{background:`url(${marvel})no-repeat center/100% 100%`}}/>
-                {/* <a className='catergory-img' style={{background:`url(${upsa})no-repeat center/100% 100%`}}/> */}
-                <a className='catergory-img brand' style={{background:`url(${sony})no-repeat center/100% 100%`}}/>
+                    <a className='catergory-img brand' style={{background:`url(${apple})no-repeat center/100% 100%`}}/>
+                    <a className='catergory-img brand' style={{background:`url(${canon})no-repeat center/100% 100%`}}/>
+                    <a className='catergory-img brand' style={{background:`url(${marvel})no-repeat center/100% 100%`}}/>
+                    {/* <a className='catergory-img' style={{background:`url(${upsa})no-repeat center/100% 100%`}}/> */}
+                    <a className='catergory-img brand' style={{background:`url(${sony})no-repeat center/100% 100%`}}/>
                
                 </div>
             </div>
 
-        </>
+        </div>
     )
 }
