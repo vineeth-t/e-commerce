611479579cfbbb9d9bc476455a01be28226a22d9
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-07-29 19:53:42 +0530

    feat: Added footer

---
+++ ecommerce/src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import './footer.css'
+
+export function Footer(){
+    return(
+        <div className='footer-container'>
+            <p >Jungle Clap By</p><br/>
+            <span> @__PVR__</span>
+        </div>
+    )
+}
