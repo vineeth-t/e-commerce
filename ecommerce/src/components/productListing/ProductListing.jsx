daaea01b2de8761ae9c82517b491747ac51b1127
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-08-02 22:32:08 +0530

    style: item's are styled using card

--- ecommerce/src/components/productListing/ProductListing.jsx
+++ ecommerce/src/components/productListing/ProductListing.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import './products.css'
 import axios from 'axios';
 export function ProductItemsListing(){
-    const[items,setItems]=useState([{}])
+    const[items,setItems]=useState([])
     useEffect(()=>async function(){
         try{
             const {data,status,statusText} = await axios.get('/api/products');
@@ -12,13 +13,20 @@ export function ProductItemsListing(){
         }
     }(),[])
 return (
-    <div>
-        Products
-        {items.map((Item)=>{
+    <div className='item-list'>
+        {items.map(({name,image,price})=>{
             return (
-                <div>
-                    {Item.name}
-                    </div>
+                <div className="card-overlay">
+                    <img style={{width: "251px",
+                    height: "251px"}} src={image} alt="cardImage"/> 
+                    <h3>{name}</h3>
+                    <h4 className="Item Item-name"> </h4>
+                    <span className="Item Item-price"> Rs.{price} </span>
+                    <s className="Item Item-discount">Rs.{price-0.5*price}</s>
+                    <span className="Item Item-discountPercentage">(50% off) </span>     
+                    <button className="wishlist-l"><svg width="1.5em" height="1.5em" viewBox="0 0 24 24"><path d="M12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3z" fill="currentColor"></path></svg></button><br/>
+                    <button className='btn-add2Cart'>Add to cart</button>
+            </div>
             )
         })}
     </div>
