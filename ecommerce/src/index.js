14d59fb767dd4b4531f7546390ef1e3a3c7b16e8
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-08-03 22:58:07 +0530

    feat: added Context
    Added context to maintain cart items gobally

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {setupMockServer} from './api/mock.server'
+import {setupMockServer} from './api/mock.server';
+import {CartProvider} from './contexts/cart-context'
 setupMockServer();
 ReactDOM.render(
   <React.StrictMode>
+    <CartProvider>
     <App />
+    </CartProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
