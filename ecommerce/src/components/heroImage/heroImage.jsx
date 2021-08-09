6f24836a5f74be63c3de83fea209de259b37e7d7
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-08-09 22:05:59 +0530

    style: mobile view is fixed

--- ecommerce/src/components/heroImage/heroImage.jsx
+++ ecommerce/src/components/heroImage/heroImage.jsx
@@ -2,14 +2,14 @@ import './hero.css';
 export function Hero(setState){
     return (
         <div>
-        <div className='heroimg'>
-           <h1>JungleClap</h1>
-           <small>A One Stop Destination For Electronic Gadgets & fashion Products </small>
-           <h1>Big Billon Day's are back</h1>
-           <small>(14-16 of August)</small>
-           <p>Offers On All Selected Items*</p>
-           <button className='btn-shp' onClick={()=>setState('Products')}>Shop Now</button>
-        </div>
+            <div className='heroimg'>
+            <h1>JungleClap</h1>
+            <small>A One Stop Destination For Electronic Gadgets & fashion Products </small>
+            <h1>Big Billon Day's are back</h1>
+            <small>(14-16 of August)</small>
+            <p>Offers On All Selected Items*</p>
+            <button className='btn-shp' onClick={()=>setState('Products')}>Shop Now</button>
+            </div>
         </div>
     )
 }
