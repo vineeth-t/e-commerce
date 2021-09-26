c917d536991165a8c60104e3d7fde43f2dbab06c
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-09-26 12:10:49 +0530

    Create React App

--- src/Routes/index.js
+++
@@ -1,5 +0,0 @@
-export {Cart} from './Cart/cartManagement'
-export {Home} from './Home/home'
-export {Wishlist} from'./Wishlist/wishlistManagement'
-export {ProductItemsListing} from './ItemListing/ProductListing'
-export {Login} from './loginCard/login'
