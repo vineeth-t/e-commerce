33b8ba3b89cd965d87c50098ec0c2545c75067bf
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-11-26 08:50:53 +0530

    fix : removed mock server
    Added Express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {setupMockServer} from './api/mock.server';
 import {StateProvider,FilterDataProvider,Auth} from './contexts/index'
 import { BrowserRouter as Router } from "react-router-dom";
-
-setupMockServer();
 ReactDOM.render(
   <React.StrictMode>
     <StateProvider>
