1f75dcd92ab2c5ea558185c0ac43b50bfb9ea0e9
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-12-20 14:50:49 +0530

    fix: api call are made, data is persisited
    added database

--- src/contexts/state-context.js
+++ src/contexts/state-context.js
@@ -1,40 +1,52 @@
 import {useEffect, createContext, useContext, useState, useReducer } from "react";
-import {stateReducer} from '../reducers/stateReducer'
+import {stateReducer} from '../reducers/stateReducer';
+import {useAuth} from './index'
 import axios from 'axios';
 export const StateContext= createContext();
 export function StateProvider({children}){
-    const[state,dispatch]=useReducer(stateReducer,{cartItems,wishListItems,toast,address:[],currentAddress:{
-      houseNo: "4-46",
-      streetName: "sree ram nagar ",
-      landmark: "vartha office",
-      city: "khammam",
-      district: "khammmam",
-      pincode:507101
-    }})
-    const[items,setItems]=useState([]);
+  const{authState:{userId}} =useAuth();
+    const[state,dispatch]=useReducer(stateReducer,{
+                                                  products,
+                                                  cartItems,
+                                                  wishListItems,
+                                                  toast,
+                                                  address:[],
+                                                  currentAddress:{
+                                                                    houseNo: "4-46",
+                                                                    streetName: "sree ram nagar ",
+                                                                    landmark: "vartha office",
+                                                                    city: "khammam",
+                                                                    district: "khammmam",
+                                                                    pincode:507101
+                                                                  }})
     const[loader,setLoader]=useState(false);
     useEffect(()=>async function(){
         setLoader(true)
         try{
-            const {data:{products}} = await axios.get('https://JungleClap-Express-Server.vineetht.repl.co/products')
-            console.log(products)
-            setItems(products)
+            const {data:{response,products}} = await axios.get('https://JungleClap-Express-Server.vineetht.repl.co/products')
+            if(response){
+              dispatch({type:'SET_PRODUCTS',payload:products})
+            }else{
+              dispatch({type:'TOAST',toast:'Refresh the Page'})
+            }
+     
         }catch(error){
             console.log(error)
         }finally{
         setLoader(false)
-    
         }
     }(),[])
     useEffect(()=>async function(){
         try{
-            const {data:{response}}=await axios.get('https://JungleClap-Express-Server.vineetht.repl.co/wishlist')
-            console.log(response)
-            dispatch({type:'SET_WISHLIST',payload:response})
+            const {data:{response,wishlistItems}}=await axios.get(`https://JungleClap-Express-Server.vineetht.repl.co/wishlist/${userId}`)
+            console.log(wishlistItems)
+            if(response){
+                dispatch({type:'SET_WISHLIST',payload:wishlistItems})
+            }
           }catch(error){
             console.log(error)
           }
-    }(),[])
+    }(),[userId])
     useEffect(()=>async function(){
         try{
             const {data:{response}}=await axios.get('https://JungleClap-Express-Server.vineetht.repl.co/cart')
@@ -45,7 +57,7 @@ export function StateProvider({children}){
           }
     }(),[])
     return(
-        <StateContext.Provider value={{state,dispatch,loader,items}}>
+        <StateContext.Provider value={{state,dispatch,loader}}>
             {children}
         </StateContext.Provider>
     )
@@ -53,6 +65,7 @@ export function StateProvider({children}){
 export function useStateContext(){
   return useContext(StateContext);
 }
+export const products=[]
 export const cartItems=[];
 export const wishListItems=[];
 export let toast;
