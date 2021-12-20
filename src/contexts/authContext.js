5a6ef11ad53ca8353bfde1cf93c1fef5604d1685
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-12-20 14:49:58 +0530

    removed unused variable

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -2,17 +2,19 @@ import { createContext, useContext, useReducer } from "react";
 import {authReducer} from '../reducers/authReducer'
 export const AuthContext=  createContext();
 export function Auth({children}){
- let login;
+ let login,userId;
  let userName='';
  const loginStatus= JSON.parse(localStorage.getItem('login'));
  if(loginStatus?.isUserLoggedIn){
-     login=true;
+     login=true
      userName=loginStatus.userName
+     userId=loginStatus.userId
  }else{
      login=false
  }
 
     const[authState,authDispatch]=useReducer(authReducer, {
+                                                      userId,
                                                       login,
                                                       userName,
                                                       password:''
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,3 @@
-export {StateProvider} from './state-context'
+export {StateProvider,useStateContext} from './state-context'
 export {useDataFilter,FilterDataProvider} from './filteringDataContext'
 export {Auth,useAuth} from'./authContext'
