1c57bb8afe77ca3527f335c555da27d7a5935252
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-01-09 14:12:33 +0530

    made axios calls using token

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@
 import {NavBar,Footer,SignUp,Login} from './components/index';
 import { Cart, Home,Wishlist,ProductItemsListing, Profile} from './Routes/index';
 import {PrivateRoute} from './Routes/PrivateRoute/privateRoute'
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import {useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import './style.css'
 import { useAuth, useStateContext } from './contexts';
-import { getCartItemsFromDB, getWishListedItemsFromDB } from './components/axios/axios';
+import { getCartItemsFromDB, getWishListedItemsFromDB } from './components/axios';
 function App() {
-
   const{dispatch}=useStateContext()
-  const{authState:{userId}} =useAuth();
-  const navigate=useNavigate()
-
+  const{authState:{token}} =useAuth();
   useEffect(()=>async function(){
-    getCartItemsFromDB(userId,dispatch,navigate);
-    getWishListedItemsFromDB(userId,dispatch,navigate)
-  }(),[userId,dispatch,navigate])
+    console.log(token)
+    if(token){
+      getCartItemsFromDB(dispatch);
+      // getWishListedItemsFromDB(token,dispatch,navigate)
+    }
+  }(),[token,dispatch])
   return (
     <div className="App">
       <div className='App-Container'>
