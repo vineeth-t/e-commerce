43d2a934df3d7f588af48449628a9798b8e18676
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-01-09 14:10:15 +0530

    added default address

--- src/components/addressCard/addressCard.js
+++ src/components/addressCard/addressCard.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useStateContext } from '../../contexts/state-context';
-
 import './addressCard.css'
 import { AddressForm } from './addressForm';
 export function AddressCard({setAddress}){
@@ -14,10 +13,8 @@ export function AddressCard({setAddress}){
           <button className='btn-edit-address' onClick={()=>setAddress(true)}>
           <svg width="2em" height="2em" viewBox="0 0 24 24"><path d="M14 11c0 .55-.45 1-1 1H4c-.55 0-1-.45-1-1s.45-1 1-1h9c.55 0 1 .45 1 1zM3 7c0 .55.45 1 1 1h9c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1zm7 8c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1h5c.55 0 1-.45 1-1zm8.01-2.13l.71-.71a.996.996 0 0 1 1.41 0l.71.71c.39.39.39 1.02 0 1.41l-.71.71l-2.12-2.12zm-.71.71l-5.16 5.16c-.09.09-.14.21-.14.35v1.41c0 .28.22.5.5.5h1.41c.13 0 .26-.05.35-.15l5.16-5.16l-2.12-2.11z" fill="currentColor"></path></svg>
           </button>
-         </div>
-   
+         </div>  
 }
-
 export function AdressModal({setAddress}){
     const{state:{address_details},dispatch}=useStateContext();
     const[openAddressForm,setAddressForm]=useState(false)
