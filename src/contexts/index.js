09b693fb15b563a891a8b53f3e1dde9e792a906c
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-01-09 14:11:27 +0530

    Update index.js

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,3 @@
 export {StateProvider,useStateContext} from './state-context'
 export {useDataFilter,FilterDataProvider} from './filteringDataContext'
-export {Auth,useAuth} from'./authContext'
+export {AuthProvider,useAuth} from'./authContext'
